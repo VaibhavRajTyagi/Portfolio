64d60e1e90af3d53caadd607567ebb4d64b2c753
VaibhavRT <vaibtyagi1610@gmail.com>
2025-01-26 02:50:56 +0530

    Added new icons on the landing page for social media links fixed few spacings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
 import profileImage from "../assets/images/pfp.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquareGithub,
+  faLinkedin,
+  faGoogle,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
+  const iconStyles = "text-gray-400 text-4xl hover:text-white";
+
   return (
     <>
-      <div className="landingDiv flex-grow h-full bg-black text-white flex justify-center items-center gap-24 m-4">
+      <div className="landingDiv flex-grow h-full bg-black text-white flex justify-center items-center gap-24 mt-20">
         <div className="TextDiv flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold">
             Hello, I&apos;m{" "}
@@ -14,6 +22,37 @@ const Home = () => {
         <div className="ImageDiv border-4 border-dotted border-white overflow-hidden object-cover w-80 h-80 rounded-full">
           <img className="bg-[#141313]" src={profileImage} alt="PFP" />
         </div>
+      </div>
+
+      <div className="socials flex justify-center items-center gap-8 my-16">
+        <button
+          onClick={() =>
+            window.open(
+              "https://www.linkedin.com/in/vaibhav-raj-tyagi-10b222270/",
+              "_blank"
+            )
+          }
+        >
+          <FontAwesomeIcon icon={faLinkedin} className={iconStyles} />
+        </button>
+        <button
+          onClick={() =>
+            window.open("https://twitter.com/your-profile", "_blank")
+          }
+        >
+          <FontAwesomeIcon icon={faGoogle} className={iconStyles} />
+        </button>
+        <button
+          onClick={() =>
+            window.open(
+              "https://github.com/VaibhavRajTyagi?tab=repositories",
+              "_blank"
+            )
+          }
+        >
+          <FontAwesomeIcon icon={faSquareGithub} className={iconStyles} />
+        </button>
+        
       </div>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,17 +3,13 @@ import { NavLink } from "react-router";
 const Nav = () => {
   const navLinkStyles = ({ isActive }) => {
     return `
-          ${isActive ? "text-black bg-white font-bold" : ""}
-          hover:text-gray-300 hover:bg-gray-900
-          transition duration-300 
-          leading-none
-          text-base
-          px-2 py-1
+          ${isActive ? "text-white" : "text-slate-500"}
+          font-[Inter]
         `;
   };
 
   return (
-    <div className="flex justify-center items-center gap-24 opacity-95 bg-black text-white pt-8 fixed top-0 left-0 right-0 z-10">
+    <div className="flex justify-center items-center gap-24 opacity-95 bg-black text-white py-6 fixed top-0 left-0 right-0 z-10">
       <NavLink to="/" className={navLinkStyles}>
         Home
       </NavLink>
