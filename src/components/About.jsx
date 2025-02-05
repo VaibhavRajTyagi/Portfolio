8e66253af8bee7798c97439b19e24b30dc3c3e67
VaibhavRT <vaibtyagi1610@gmail.com>
2025-02-05 23:53:30 +0530

    Changed the font of the about section to mono.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,8 @@ const About = () => {
       <div className='heading  text-4xl text-center mx-4 my-12 md:mx-8'>
         <h1>About</h1>
       </div>
-      <div className="disc m-4 px-4 text-center font-mono">
-        <p>Hey, I&apos;m Vaibhav a 2024 Masters Of Computer Applications graduate from the College Of Engineering Roorkee. I&apos;m a beginner full-stack engineer, with particular proficiency in ReactJS and front-end. I build and design vercel like minimalist and modern looking websites. I&apos;m still in the learning process and will continually keep upgrading my projects along with my knowledge and skills.</p>
+      <div className="disc m-4 px-4 text-center">
+        <p className='font-mono'>Hey, I&apos;m Vaibhav a 2024 Masters Of Computer Applications graduate from the College Of Engineering Roorkee. I&apos;m a beginner full-stack engineer, with particular proficiency in ReactJS and front-end. I build and design vercel like minimalist and modern looking websites. I&apos;m still in the learning process and will continually keep upgrading my projects along with my knowledge and skills.</p>
       </div>
       <div className="education">
         <EducationTimeline />
